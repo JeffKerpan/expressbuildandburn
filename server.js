d721c13b2d4c03ca58b2288b58cae02e0342bd5e
Jeff Kerpan <jkerpan@gmail.com>
2017-05-27 13:17:59 -0600

    GET request showing on local host

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', function(req, res) {
+  res.send('HELLO SIMPLE SERVER!');
+});
+
 app.listen(3000, function() {
   console.log('Listening on port: ' + port);
 });
