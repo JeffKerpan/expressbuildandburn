e91ceb0435088ceb514d2a1bc6059cd2dd673880
Jeff Kerpan <jkerpan@gmail.com>
2017-05-27 13:21:33 -0600

    Displays static html page

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
   res.send('HELLO SIMPLE SERVER!');
 });
+
 
 app.listen(3000, function() {
   console.log('Listening on port: ' + port);
